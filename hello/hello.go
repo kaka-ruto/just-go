5d61ef61014ccf0876d1d62f1debf76ccaaacb83
Kaka Ruto <borecollins@gmail.com>
2019-06-08 14:54:14 +0300

    Test I can pass arguments to the function

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+const englishPrefixName = "Hello, "
+
+func Hello(name string) string {
+	return englishPrefixName + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
