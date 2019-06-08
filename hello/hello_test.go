e5788cb1f9313344f82b42954d38f62fef020745
Kaka Ruto <borecollins@gmail.com>
2019-06-08 15:26:16 +0300

    Refactor tests to be DRY

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,22 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // Tell the suite this is a helper, and the test should not fail here
 
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
