fa7b6ad426ac4de890d854a31a6fdb0d2b01f96f
Kaka Ruto <borecollins@gmail.com>
2019-06-09 22:42:09 +0300

    Add ExampleAdd to show/document how to use our code

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,12 +1,21 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
 
 	if sum != expected {
-		t.ErrorF("expected '%d' but got '%d'", expected, sum)
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
