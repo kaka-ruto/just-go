d19958c99137644d8cea48839c9a66386f1eacd7
Kaka Ruto <borecollins@gmail.com>
2019-06-10 14:11:18 +0300

    Add ExampleRepeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,6 +2,7 @@ package iteration
 
 const repeatCount = 5
 
+// Returns character repeated 5 times
 func RepeatFiveTimes(character string) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := RepeatFiveTimes("a")
@@ -9,4 +12,16 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected '%s' got '%s'", expected, repeated)
 	}
+}
+
+func BenchMarkRepeatFiveTimes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RepeatFiveTimes("a")
+	}
+}
+
+func ExampleRepeatFiveTimes() {
+	repeated := RepeatFiveTimes("g")
+	fmt.Println(repeated)
+	// Output: "ggggg"
 }
