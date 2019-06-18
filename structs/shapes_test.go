c96692e0cb65eee1a6cb3d3697979f5c1c7fc194
Kaka Ruto <borecollins@gmail.com>
2019-06-18 20:47:31 +0300

    Use struct Rectangle instead of float64

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,8 +13,9 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
