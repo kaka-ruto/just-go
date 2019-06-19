31829713e3ef578c8e662e7d5fbcb956e492697f
Kaka Ruto <borecollins@gmail.com>
2019-06-19 16:46:01 +0300

    Add the Triangle shape

--- structs/shapes.go
+++ structs/shapes.go
@@ -6,8 +6,17 @@ type Shape interface {
 	Area() float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 type Rectangle struct {
 	Width  float64
+	Height float64
+}
+
+type Triangle struct {
+	Base   float64
 	Height float64
 }
 
@@ -15,12 +24,12 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-type Circle struct {
-	Radius float64
-}
-
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func Perimeter(rectangle Rectangle) float64 {
