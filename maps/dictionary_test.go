adea02efeb1fd271328b1f575527e3a675fc3888
Kaka Ruto <borecollins@gmail.com>
2019-06-21 02:16:00 +0300

    Use an assertStrings helper to make test implementation more general

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -8,6 +8,12 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "Time")
 	expected := "To Go"
 
+	assertStrings(t, got, expected)
+}
+
+func assertStrings(t *testing.T, got, expected string) {
+	t.Helper()
+
 	if got != expected {
 		t.Errorf("got %s expected %s given %s", got, expected, "Time")
 	}
