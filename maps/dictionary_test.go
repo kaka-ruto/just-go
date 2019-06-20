3f19423a1a38532d87b753bde31e404bb3faf7e5
Kaka Ruto <borecollins@gmail.com>
2019-06-21 02:49:28 +0300

    Show an error when a word is not found

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"Time": "To Go"}
 
-	got := dictionary.Search("Time")
-	expected := "To Go"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("Time")
+		expected := "To Go"
 
-	assertStrings(t, got, expected)
+		assertStrings(t, got, expected)
+	})
+
+	t.Run("unbeknownst to man", func(t *testing.T) {
+		_, err := dictionary.Search("Unbeknownst")
+		expected := "Couldn't find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("Couldn't find the word")
+		}
+
+		assertStrings(t, err.Error(), expected)
+	})
 }
 
 func assertStrings(t *testing.T, got, expected string) {
