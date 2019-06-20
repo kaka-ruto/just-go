54d9dfd3ac151bf2526e5b334fcd3a2712418221
Kaka Ruto <borecollins@gmail.com>
2019-06-20 18:39:14 +0300

    Show how in Go, when you call a function or a method, the arguments are copied. This results in values from different storage addresses

---
+++ pointers/wallet_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
